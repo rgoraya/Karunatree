58bf58666ac04ca8e71a433ae12a56c3b1aba658
mike <mike@e4c21465-b175-4386-9795-66fa23ef8cd2>
2010-03-20 05:25:08 +0000

    added page seeds

--- app/models/seedling.rb
+++ app/models/seedling.rb
@@ -1,7 +1,15 @@
+require 'rubygems'
+require 'mini_magick'
+
 class Seedling < ActiveRecord::Base
-  has_attached_file :picture, :styles => {:large => "800x600>", :medium => "300x300>", :thumb => "100x100>" } #, :processors => [:tiff_formatter]
+  has_attached_file :picture, :styles => {:large => "800x600>", :medium => "300x300>", :thumb => "100x100>" }#, :processors => [:tiff_formatter]
+  before_post_process :convert_tiff_to_png
   
   belongs_to :user
   
   validates_attachment_size :picture, :less_than => 10.megabyte
+  
+  def convert_tiff_to_png
+    new_image = MiniMagick::Image.from_file(@file.path)
+  end
 end
--- lib/paperclip_processors/tiff_formatter.rb
+++ lib/paperclip_processors/tiff_formatter.rb
@@ -2,25 +2,26 @@ require 'rubygems'
 require 'mini_magick'
 
 module Paperclip
-  class TiffFormatter < Processor
+  class TiffFormatter < Paperclip::Processor
     def initialize(file, options = {}, attachment = nil)
+      super
       @file = file
       @attachment = attachment
+      @instance = options[:instance]
       @format = File.extname(@file.path)
+      @current_format   = File.extname(@file.path)
+      @basename         = File.basename(@file.path, @current_format)
     end
     
-    def make
-      if @format == ".tiff"
-        # MiniMagick::Image.from_file(@file)
-        puts "zomg"
-        @file
-      else
-        puts "it not worky"
-        new_image = MiniMagick::Image.from_file(@file.path)
-        new_image.format "png"
-        new_image.write @file.path
-        new_image
+    def make      
+      @file.pos = 0 # Reset the file position incase it is coming out of a another processor
+      dst = Tempfile.new([@basename, @format].compact.join("."))
+      new_image = MiniMagick::Image.from_file(@file.path)
+      if @format == ".tiff" || @format == ".tif"
+        new_image.format 'tiff', '*.png'
       end
+      dst << new_image
+      dst
     end
   end
 end
